63a94748450575022a98ca6f015e76828d406b99
pedrovitors <pedrovitorfhs@gmail.com>
2021-07-10 17:54:59 -0300

    Added router setup

---
+++ src/routes/Router.js
@@ -0,0 +1,43 @@
+import React from "react"
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import MusicPage from "../pages/MusicPage/MusicPage";
+import MusicDetailsPage from "../pages/MusicDetailsPage/MusicDetailsPage";
+import SignupPage from "../pages/SignupPage/SignupPage";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import AddMusic from "../pages/AddMusic/AddMusic";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/login">
+                    <LoginPage/>
+                </Route>
+
+                <Route exact path="/signup">
+                    <SignupPage/>
+                </Route>
+
+                <Route exact path="/">
+                    <MusicPage/>
+                </Route>
+
+                <Route exact path="/add-music">
+                    <AddMusic/>
+                </Route>
+
+                <Route exact path="/music-details/:id">
+                    <MusicDetailsPage/>
+                </Route>
+
+                <Route exact path="">
+                    <ErrorPage/>
+                </Route>
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router
